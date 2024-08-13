71331bbae15e16dd2cc1649b349f29f1231d7c35
GuilhermeFiorot <guilhermefirme3@gmail.com>
2024-08-13 10:04:07 -0300

    add: acresentarRota, adicionarAresta e obterAresta

--- src/lib/Grafo.java
+++ src/lib/Grafo.java
@@ -11,3 +11,30 @@ public class Grafo <T> {
         this.vertices = new ArrayList<Vertice<T>>();
     }
 }
+
+public void adicionarAresta(T origem, T destino, float peso) {
+    Vertice origemVert, destinoVert;
+    Aresta novaAresta;
+
+    origemVert = this.obterVertice(origem);
+    if (origemVert == null) {
+        origemVert = this.adicionarVertice(origem);
+    }
+
+    destinoVert = obterVertice(destino);
+    if (destinoVert == null) {
+        destinoVert = this.adicionarVertice(destino);
+    }
+
+    novaAresta = new Aresta(origemVert, destinoVert, peso);
+    this.arestas.add(novaAresta);
+}
+
+public Aresta obterAresta(Vertice<T> origem, Vertice<T> destino) {
+    for (Aresta aresta : arestas) {
+        if (aresta.getOrigem().equals(origem) && aresta.getDestino().equals(destino)) {
+            return aresta;
+        }
+    }
+    return null; // Retorna null se a aresta não for encontrada
+}
